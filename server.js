349a0b759ac782409bc6f811e496209195d9c0a9
Jeanne d'Arc <njoanc@gmail.com>
2021-04-26 17:36:13 +0200

    feat(): deployment to heroku

--- server.js
+++ server.js
@@ -1,32 +1,22 @@
-// server.js
-// where your node app starts
+const express = require("express");
+const app = express();
+require("dotenv").config();
+// listen for requests :)
+var port = process.env.PORT || 3000;
 
-// init project
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static(__dirname + "/public"));
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
-
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
