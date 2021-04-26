3eefbed49add7a228e463a56fd108e41d0b029bf
Jeanne d'Arc <njoanc@gmail.com>
2021-04-26 17:44:06 +0200

    change(): styling

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-// listen for requests :)
+// listen port for requests :)
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`This app is listening at http://localhost:${port}`);
 });
